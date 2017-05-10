d8dda336d71c197d2b415eec6e1bd209e394d6e4
ospontosdaliga <ospontosdaliga@gmail.com>
2017-05-10 15:49:29 +0100

    Jornada 31

--- js/20162017/cinfo.js
+++ js/20162017/cinfo.js
@@ -1,18 +1,19 @@
-﻿function getCoachIds() { return [137,205,422,607,777,780,781,117,453,134,494,133,390,132,223,634,763]; }
-function getCoachInfoOf137() { return {name:'Pedro Martins', team:'V. Guimarães', roundPoints:'6', totalPoints:'111', averagePointsPerRound:'4.11', rank:'', rankDescription:''}; }
-function getCoachInfoOf205() { return {name:'Daniel Ramos', team:'Marítimo', roundPoints:'1', totalPoints:'101', averagePointsPerRound:'3.74', rank:'', rankDescription:''}; }
-function getCoachInfoOf422() { return {name:'Miguel Leal', team:'Boavista', roundPoints:'5', totalPoints:'67', averagePointsPerRound:'2.48', rank:'', rankDescription:''}; }
-function getCoachInfoOf607() { return {name:'Luís Castro', team:'Rio Ave', roundPoints:'6', totalPoints:'67', averagePointsPerRound:'2.48', rank:'', rankDescription:''}; }
-function getCoachInfoOf777() { return {name:'Vasco Seabra', team:'P. Ferreira', roundPoints:'6', totalPoints:'48', averagePointsPerRound:'1.78', rank:'', rankDescription:''}; }
-function getCoachInfoOf780() { return {name:'Ricardo Soares', team:'Chaves', roundPoints:'0', totalPoints:'47', averagePointsPerRound:'1.74', rank:'', rankDescription:''}; }
-function getCoachInfoOf781() { return {name:'Nuno Manta Santos', team:'Feirense', roundPoints:'5', totalPoints:'63', averagePointsPerRound:'2.33', rank:'', rankDescription:''}; }
-function getCoachInfoOf117() { return {name:'Pedro Emanuel', team:'Estoril', roundPoints:'3', totalPoints:'23', averagePointsPerRound:'0.85', rank:'', rankDescription:''}; }
-function getCoachInfoOf453() { return {name:'João de Deus', team:'Nacional', roundPoints:'0', totalPoints:'7', averagePointsPerRound:'0.26', rank:'', rankDescription:''}; }
-function getCoachInfoOf134() { return {name:'Domingos Paciência', team:'Belenenses', roundPoints:'1', totalPoints:'0', averagePointsPerRound:'0', rank:'', rankDescription:''}; }
-function getCoachInfoOf494() { return {name:'Abel Ferreira', team:'Sp. Braga', roundPoints:'2', totalPoints:'2', averagePointsPerRound:'0.07', rank:'', rankDescription:''}; }
-function getCoachInfoOf133() { return {name:'Jorge Jesus', team:'Sporting', roundPoints:'7', totalPoints:'111', averagePointsPerRound:'4.11', rank:'', rankDescription:''}; }
-function getCoachInfoOf390() { return {name:'Nuno Espírito Santo', team:'FC Porto', roundPoints:'5', totalPoints:'140', averagePointsPerRound:'5.19', rank:'', rankDescription:''}; }
-function getCoachInfoOf132() { return {name:'Rui Vitória', team:'Benfica', roundPoints:'4', totalPoints:'120', averagePointsPerRound:'4.44', rank:'', rankDescription:''}; }
-function getCoachInfoOf223() { return {name:'José Couceiro', team:'V. Setúbal', roundPoints:'1', totalPoints:'74', averagePointsPerRound:'2.74', rank:'', rankDescription:''}; }
-function getCoachInfoOf634() { return {name:'Petit', team:'Moreirense', roundPoints:'5', totalPoints:'35', averagePointsPerRound:'1.3', rank:'', rankDescription:''}; }
-function getCoachInfoOf763() { return {name:'Pepa', team:'Tondela', roundPoints:'-1', totalPoints:'36', averagePointsPerRound:'1.33', rank:'', rankDescription:''}; }
+﻿function getCoachIds() { return [137,205,422,607,783,777,780,781,117,453,134,494,133,390,132,223,634,763]; }
+function getCoachInfoOf137() { return {name:'Pedro Martins', team:'V. Guimarães', roundPoints:'5', totalPoints:'116', averagePointsPerRound:'4.14', rank:'', rankDescription:''}; }
+function getCoachInfoOf205() { return {name:'Daniel Ramos', team:'Marítimo', roundPoints:'6', totalPoints:'107', averagePointsPerRound:'3.82', rank:'', rankDescription:''}; }
+function getCoachInfoOf422() { return {name:'Miguel Leal', team:'Boavista', roundPoints:'4', totalPoints:'71', averagePointsPerRound:'2.54', rank:'', rankDescription:''}; }
+function getCoachInfoOf607() { return {name:'Luís Castro', team:'Rio Ave', roundPoints:'1', totalPoints:'68', averagePointsPerRound:'2.43', rank:'', rankDescription:''}; }
+function getCoachInfoOf783() { return {name:'Jorge Leitão', team:'Arouca', roundPoints:'0', totalPoints:'0', averagePointsPerRound:'0', rank:'', rankDescription:''}; }
+function getCoachInfoOf777() { return {name:'Vasco Seabra', team:'P. Ferreira', roundPoints:'1', totalPoints:'49', averagePointsPerRound:'1.75', rank:'', rankDescription:''}; }
+function getCoachInfoOf780() { return {name:'Ricardo Soares', team:'Chaves', roundPoints:'2', totalPoints:'49', averagePointsPerRound:'1.75', rank:'', rankDescription:''}; }
+function getCoachInfoOf781() { return {name:'Nuno Manta Santos', team:'Feirense', roundPoints:'5', totalPoints:'68', averagePointsPerRound:'2.43', rank:'', rankDescription:''}; }
+function getCoachInfoOf117() { return {name:'Pedro Emanuel', team:'Estoril', roundPoints:'7', totalPoints:'30', averagePointsPerRound:'1.07', rank:'', rankDescription:''}; }
+function getCoachInfoOf453() { return {name:'João de Deus', team:'Nacional', roundPoints:'3', totalPoints:'10', averagePointsPerRound:'0.36', rank:'', rankDescription:''}; }
+function getCoachInfoOf134() { return {name:'Domingos Paciência', team:'Belenenses', roundPoints:'7', totalPoints:'7', averagePointsPerRound:'0.25', rank:'', rankDescription:''}; }
+function getCoachInfoOf494() { return {name:'Abel Ferreira', team:'Sp. Braga', roundPoints:'1', totalPoints:'3', averagePointsPerRound:'0.11', rank:'', rankDescription:''}; }
+function getCoachInfoOf133() { return {name:'Jorge Jesus', team:'Sporting', roundPoints:'0', totalPoints:'111', averagePointsPerRound:'3.96', rank:'', rankDescription:''}; }
+function getCoachInfoOf390() { return {name:'Nuno Espírito Santo', team:'FC Porto', roundPoints:'2', totalPoints:'142', averagePointsPerRound:'5.07', rank:'', rankDescription:''}; }
+function getCoachInfoOf132() { return {name:'Rui Vitória', team:'Benfica', roundPoints:'6', totalPoints:'126', averagePointsPerRound:'4.5', rank:'', rankDescription:''}; }
+function getCoachInfoOf223() { return {name:'José Couceiro', team:'V. Setúbal', roundPoints:'1', totalPoints:'75', averagePointsPerRound:'2.68', rank:'', rankDescription:''}; }
+function getCoachInfoOf634() { return {name:'Petit', team:'Moreirense', roundPoints:'8', totalPoints:'43', averagePointsPerRound:'1.54', rank:'', rankDescription:''}; }
+function getCoachInfoOf763() { return {name:'Pepa', team:'Tondela', roundPoints:'5', totalPoints:'41', averagePointsPerRound:'1.46', rank:'', rankDescription:''}; }
